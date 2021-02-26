f193336aac072c9472be853ba3413534d8150083
Ovidiu Muntean <ovidiu.muntean@imawo.ro>
2021-02-26 01:32:52 -0800

    Initial Commit

---
+++ seven.seconds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seven.seconds
+//
+//  Created by Ovidiu Muntean on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
